226b253a39c46ea39c404de1d2bd9a8a917f4efa
bildungsromanx <159861856+bildungsromanx@users.noreply.github.com>
2024-04-12 22:50:02 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
+window.onload = () => {
+  const generateRandomNumber = () => {
+    const numbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let indexNumbers = Math.floor(Math.random() * (numbers.length - 1));
+    return numbers[indexNumbers];
+  };
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const generateRandomSuit = () => {
+    const suit = ["diamond", "heart", "club", "spade"];
+    let indexSuit = Math.floor(Math.random() * (suit.length - 1));
+    console.log(indexSuit);
+    return suit[indexSuit];
+  };
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const myBtn = document.querySelector(".button");
+  myBtn.addEventListener("click", function() {
+    document.querySelector(".card").innerHTML = generateRandomNumber();
+    document.querySelector(".card").classList.add(generateRandomSuit());
+    console.log(generateRandomSuit());
+  });
 };
